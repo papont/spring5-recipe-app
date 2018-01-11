323bfced3822c07d31614e475775c30737a421af
papont <trusoff.a@yandex.ru>
2018-01-11 17:17:02 +0400

    Add main controller

---
+++ src/main/java/ru/papont/spring5recipeapp/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package ru.papont.spring5recipeapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Main controller
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
