f427d086ddd0fd1d81f25fc74c933d982e0f09c4
papont <trusoff.a@yandex.ru>
2018-01-12 15:59:49 +0400

    Add difficulty

---
+++ src/main/java/ru/papont/spring5recipeapp/model/Difficulty.java
@@ -0,0 +1,9 @@
+package ru.papont.spring5recipeapp.model;
+
+/**
+ * Created by user on 12.01.2018.
+ */
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/ru/papont/spring5recipeapp/model/Recipe.java
+++ src/main/java/ru/papont/spring5recipeapp/model/Recipe.java
@@ -17,10 +17,13 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-//    private  Difficulty difficulty;
+
+    @Enumerated(value = EnumType.STRING)
+    private  Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
+
     @Lob
     private  Byte[] image;
 
@@ -35,6 +38,22 @@ public class Recipe {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 
     public String getDescription() {
