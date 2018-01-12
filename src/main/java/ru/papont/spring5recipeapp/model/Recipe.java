e623358c375f7eb179d61c9747a714d7d1caac45
papont <trusoff.a@yandex.ru>
2018-01-12 15:48:45 +0400

    Add model class

---
+++ src/main/java/ru/papont/spring5recipeapp/model/Recipe.java
@@ -0,0 +1,111 @@
+package ru.papont.spring5recipeapp.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+    private Integer propName;
+    private Integer cookName;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+//    private  Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
+    @Lob
+    private  Byte[] image;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Notes notes;
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPropName() {
+        return propName;
+    }
+
+    public void setPropName(Integer propName) {
+        this.propName = propName;
+    }
+
+    public Integer getCookName() {
+        return cookName;
+    }
+
+    public void setCookName(Integer cookName) {
+        this.cookName = cookName;
+    }
+
+    public Integer getServings() {
+        return servings;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDirections() {
+        return directions;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public Byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public Notes getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Notes notes) {
+        this.notes = notes;
+    }
+}
