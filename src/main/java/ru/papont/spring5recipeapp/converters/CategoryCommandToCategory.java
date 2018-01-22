f10fb462b8885df930ab84906dab6fb2b4bd914d
papont <trusoff.a@yandex.ru>
2018-01-23 00:32:34 +0400

    add Commands and Converters

---
+++ src/main/java/ru/papont/spring5recipeapp/converters/CategoryCommandToCategory.java
@@ -0,0 +1,26 @@
+package ru.papont.spring5recipeapp.converters;
+
+import ru.papont.spring5recipeapp.commands.CategoryCommand;
+import ru.papont.spring5recipeapp.model.Category;
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategoryCommandToCategory implements Converter<CategoryCommand, Category>{
+
+    @Synchronized
+    @Nullable
+    @Override
+    public Category convert(CategoryCommand source) {
+        if (source == null) {
+            return null;
+        }
+
+        final Category category = new Category();
+        category.setId(source.getId());
+        category.setDescription(source.getDescription());
+        return category;
+    }
+}
---
+++ src/main/java/ru/papont/spring5recipeapp/converters/CategoryToCategoryCommand.java
@@ -0,0 +1,28 @@
+package ru.papont.spring5recipeapp.converters;
+
+import lombok.Synchronized;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import ru.papont.spring5recipeapp.commands.CategoryCommand;
+import ru.papont.spring5recipeapp.model.Category;
+
+@Component
+public class CategoryToCategoryCommand implements Converter<Category, CategoryCommand> {
+
+    @Synchronized
+    @Nullable
+    @Override
+    public CategoryCommand convert(Category source) {
+        if (source == null) {
+            return null;
+        }
+
+        final CategoryCommand categoryCommand = new CategoryCommand();
+
+        categoryCommand.setId(source.getId());
+        categoryCommand.setDescription(source.getDescription());
+
+        return categoryCommand;
+    }
+}
